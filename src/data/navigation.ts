9d7c6731b7971a003bb5b963798bf3cb06bf4895
Artem Khrustalev <khru-artem@yandex.ru>
2026-07-21 01:50:20 +0300

    feat:начал работу над меню

---
+++ src/data/navigation.ts
@@ -0,0 +1,23 @@
+import type { HTMLAttributeAnchorTarget } from "react";
+
+export type MenuItem = {
+  label: string;
+  href: string;
+
+  target?: HTMLAttributeAnchorTarget;
+};
+
+export const navigation: MenuItem[] = [
+  {
+    label: "Что мы умеем",
+    href: "#services",
+  },
+  {
+    label: "Кейсы",
+    href: "#cases",
+  },
+  {
+    label: "Клиенты",
+    href: "#clients",
+  },
+];
