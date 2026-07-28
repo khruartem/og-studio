3564bec43d57760cd52dbc2b83f7fc959f411580
Artem Khrustalev <khru-artem@yandex.ru>
2026-07-29 00:08:31 +0300

    feat:добавил утилиты блокировки скрола

--- src/components/ui/MobileMenu/mobile-menu-controller.ts
+++ src/components/ui/MobileMenu/mobile-menu-controller.ts
@@ -1,12 +1,16 @@
+import { scrollLock } from "../../../lib/dom/lock-scroll";
+
 export const createMobileMenu = (menu: HTMLElement) => {
   const trigger = menu.querySelector<HTMLButtonElement>("[data-trigger]");
 
   function open() {
+    scrollLock.lock();
     menu.dataset.state = "open";
     trigger?.setAttribute("aria-expanded", "true");
   }
 
   function close() {
+    scrollLock.unlock();
     menu.dataset.state = "closed";
     trigger?.setAttribute("aria-expanded", "false");
   }
