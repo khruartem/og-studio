1683b93a81bab290cc98b06517dbd1ae25e8edc3
Artem Khrustalev <khru-artem@yandex.ru>
2026-08-09 00:02:41 +0300

    feat:начал работу над секцией о нас

---
+++ src/data/categories.ts
@@ -0,0 +1,35 @@
+import type { CategoryVariant } from "../components/ui/CategoryCard.astro";
+
+import theatre from "../assets/categories-bg/theatre.jpg";
+import music from "../assets/categories-bg/music.jpg";
+import designers from "../assets/categories-bg/designers.jpg";
+import blogers from "../assets/categories-bg/blogers.jpg";
+
+type Category = {
+  variant: CategoryVariant;
+  image: string;
+  title: string;
+};
+
+export const categories: Category[] = [
+  {
+    variant: "theatre",
+    image: theatre.src,
+    title: "Театр и кино",
+  },
+  {
+    variant: "music",
+    image: music.src,
+    title: "Музыкальные бренды",
+  },
+  {
+    variant: "designers",
+    image: designers.src,
+    title: "Дизайнеры и художники",
+  },
+  {
+    variant: "blogers",
+    image: blogers.src,
+    title: "Блоггеры и подкасты",
+  },
+];
