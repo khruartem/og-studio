ce776ce80c57f9d4673aa31dfe2ce368b1782985
Artem Khrustalev <khru-artem@yandex.ru>
2026-08-11 23:50:51 +0300

    feat:вынес все скрипты в единую папку и создал единую точку входа init

--- src/scripts/init.ts
+++ src/scripts/init.ts
@@ -1,9 +1,11 @@
-import { createMobileMenu } from "./mobile-menu-controller";
+import { createMobileMenu } from "./controllers/mobile-menu-controller";
 
-export function initMobileMenu() {
+const init = (): void => {
   document
     .querySelectorAll<HTMLElement>("[data-mobile-menu]")
     .forEach((menu) => {
       createMobileMenu(menu).init();
     });
-}
+};
+
+init();
